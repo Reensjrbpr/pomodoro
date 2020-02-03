7a3e3472d215b4c2f2ac50a4e2c0abea02dd7cd3
Reensjrbpr <brianreens@gmail.com>
2020-02-02 23:55:14 -0500

    Added event listeners for break time buttons

--- timer.js
+++ timer.js
@@ -7,6 +7,7 @@ const brk = document.querySelector('#break');
 sess.textContent = sessTime;
 brk.textContent = breakTime;
 
+//Change timer display based on +/- being clicked
 const sessAdd = document.querySelector('#sessAdd');
 
 sessAdd.addEventListener('click', () => {
@@ -23,8 +24,30 @@ const sessMinus = document.querySelector('#sessMinus');
 sessMinus.addEventListener('click', () => {
     const sess = document.querySelector('#session');
     const timer = document.querySelector('#timer');
-    sessTime--;
-    sess.textContent = sessTime;
+    
+    if(sessTime > 0) {
+        sessTime--;
+        sess.textContent = sessTime;
+        timer.textContent = `${sessTime}:00`;
+    }
+});
 
-    timer.textContent = `${sessTime}:00`;
+//Change break time display based on +/- being clicked
+const breakAdd = document.querySelector('#breakAdd');
+
+breakAdd.addEventListener('click', () => {
+    const brk = document.querySelector('#break');
+    breakTime++;
+    brk.textContent = breakTime;
+});
+
+const breakMinus = document.querySelector('#breakMinus');
+
+breakMinus.addEventListener('click', () => {
+    const brk = document.querySelector('#break');
+
+    if(breakTime > 0) {
+        breakTime--;
+        brk.textContent = breakTime;
+    }
 });
